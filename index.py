c6da528f1147c860b0ecc3d898d5a9c34945fc8f
JamesKennedy26 <156347134+JamesKennedy26@users.noreply.github.com>
2024-05-16 14:09:17 -0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+def intro():
+    import streamlit as st
+    st.title("Select a name to begin!")
+
+def John_Backus():
+    import streamlit as st
+    st.title("John Backus")
+    st.write('  John Warner Backus (December 3, 1924 – March 17, 2007) was an American computer scientist. He led the team that invented and implemented FORTRAN, the first widely used high-level programming language, and was the inventor of the Backus–Naur form (BNF), a widely used notation to define syntaxes of formal languages. He later did research into the function-level programming paradigm, presenting his findings in his influential 1977 Turing Award lecture "Can Programming Be Liberated from the von Neumann Style?"')
+    st.write('  The IEEE awarded Backus the W. W. McDowell Award in 1967 for the development of FORTRAN. He received the National Medal of Science in 1975 and the 1977 Turing Award "for profound, influential, and lasting contributions to the design of practical high-level programming systems, notably through his work on FORTRAN, and for publication of formal procedures for the specification of programming languages".')
+    st.image('Backus.jpg', caption='John Backus', width=200)
+
+def Bjarne_Stroustrup():
+    import streamlit as st
+    st.title("Bjarne Stroustrup")
+    st.write('  Bjarne Stroustrup (/ˈbjɑːrnə ˈstrɒvstrʊp/; Danish: [ˈpjɑːnə ˈstʁʌwˀstʁɔp]; born 30 December 1950) is a Danish computer scientist, most notable for the invention and development of the C++ programming language. Stroustrup served as a visiting professor of computer science at Columbia University beginning in 2014, where he has been a full professor since 2022."')
+    st.write('  He attended Aarhus University from 1969 to 1975 and graduated with a Candidatus Scientiarum in mathematics with computer science. His interests focused on microprogramming and machine architecture. He learned the fundamentals of object-oriented programming from its inventor, Kristen Nygaard, who frequently visited Aarhus.')
+    st.image('Stroustrup.jpg', caption='Bjarne Stroustrup', width=200)
+
+def Bill_Gates():
+    import streamlit as st
+    st.title("Bill Gates")
+    st.write('  William Henry Gates III (born October 28, 1955) is an American businessman, investor, philanthropist, and writer best known for co-founding the software giant Microsoft, along with his childhood friend Paul Allen. During his career at Microsoft, Gates held the positions of chairman, chief executive officer (CEO), president, and chief software architect, while also being its largest individual shareholder until May 2014. He was a pioneer of the microcomputer revolution of the 1970s and 1980s.')
+    st.write(' Gates was born and raised in Seattle, Washington. In 1975, he and Allen founded Microsoft in Albuquerque, New Mexico. Gates led the company as its chairman and chief executive officer until stepping down as CEO in January 2000, succeeded by Steve Ballmer, but he remained chairman of the board of directors and became chief software architect. During the late 1990s, he was criticized for his business tactics, which were considered anti-competitive. This opinion has been upheld by numerous court rulings. In June 2008, Gates transitioned into a part-time role at Microsoft and full-time work at the Bill & Melinda Gates Foundation, the private charitable foundation he and his then-wife Melinda had established in 2000. He stepped down as chairman of the Microsoft board in February 2014 and assumed the role of technology adviser to support newly appointed CEO Satya Nadella. In March 2020, Gates left his board positions at Microsoft and Berkshire Hathaway to focus on his philanthropic efforts on climate change, global health and development, and education.')
+    st.image('Gates.jpg', caption='Bill Gates', width=200)
+
+def Larry_Page():
+    import streamlit as st
+    st.title("Larry Page")
+    st.write('  Lawrence Edward Page (born March 26, 1973) is an American businessman, computer scientist, and internet entrepreneur best known for co-founding Google with Sergey Brin. Page was chief executive officer of Google from 1997 until August 2001 when he stepped down in favor of Eric Schmidt, and then again from April 2011 until July 2015 when he became CEO of its newly formed parent organization Alphabet Inc.. He held that post until December 4, 2019, when he and Brin stepped down from all executive positions and day-to-day roles within the company. He remains an Alphabet board member, employee, and controlling shareholder.')
+    st.write('  Page has an estimated net worth of $125 billion as of March 2024, according to the Bloomberg Billionaires Index, and $122.8 billion according to Forbes, making him the ninth-richest person in the world. He has also invested in flying car startups Kitty Hawk and Opener. Page is the co-creator and namesake of PageRank, a search ranking algorithm for Google for which he received the Marconi Prize in 2004 along with co-writer Brin.')
+    st.image('Page.jpg', caption='Larry Page', width=200)
+
+def Douglas_Engelbart():
+    import streamlit as st
+    st.title("Douglas Engelbart")
+    st.write('  Douglas Carl Engelbart (January 30, 1925 – July 2, 2013) was an American engineer and inventor, and an early computer and Internet pioneer. He is best known for his work on founding the field of human–computer interaction, particularly while at his Augmentation Research Center Lab in SRI International, which resulted in creation of the computer mouse, and the development of hypertext, networked computers, and precursors to graphical user interfaces. These were demonstrated at The Mother of All Demos in 1968. Engelbarts law, the observation that the intrinsic rate of human performance is exponential, is named after him.')
+    st.write(' NLS, the "oN-Line System", developed by the Augmentation Research Center under Engelbarts guidance with funding primarily from ARPA (as DARPA was then known), demonstrated numerous technologies, most of which are now in widespread use; it included the computer mouse, bitmapped screens, hypertext; all of which were displayed at "The Mother of All Demos" in 1968. The lab was transferred from SRI to Tymshare in the late 1970s, which was acquired by McDonnell Douglas in 1984, and NLS was renamed Augment (now the Doug Engelbart Institute).[6] At both Tymshare and McDonnell Douglas, Engelbart was limited by a lack of interest in his ideas and funding to pursue them and retired in 1986.')
+    st.image('Engelbart.jpg', caption='Douglas Engelbart', width=200)
+
+def Robert_Gaskins():
+    import streamlit as st
+    st.title("Robert Gaskins")
+    st.write('  Robert Gaskins invented PowerPoint. He led its initial design and development at a startup, where the idea attracted the first venture capital investment ever made by Apple Computer. PowerPoint was released for Macintosh in 1987, and soon afterward, it became the first significant acquisition ever made by Microsoft, who set up a new business unit in Silicon Valley to develop it further. Gaskins headed this new Microsoft unit for another five years, completing versions of the PowerPoint product which contributed to the explosive early growth of Microsoft Windows and to the dominance of Microsoft Office.')
+    st.write('  Gaskins was educated in Computer Science at University of California, Berkeley, and subsequently did interdisciplinary graduate study in literature and computing.In the early 1980s Gaskins worked five years as manager of computer science research at Bell Northern Research, an international telecommunications R&D laboratory in Silicon Valley. Subsequently, he joined Forethought, Inc., where the development of PowerPoint was begun.Gaskins was the entrepreneur behind the development of PowerPoint, later known as Microsoft PowerPoint after acquisition by Microsoft in the early 1990s. Lee Gomes wrote in The Wall Street Journal Robert Gaskins was the visionary entrepreneur who in the mid-1980s realized that the huge but largely invisible market for preparing business slides was a perfect match for the coming generation of graphics-oriented computers.Many original documents written by Robert Gaskins during the early history of PowerPoints strategy and development are online for public access.After leaving Microsoft, Gaskins became somewhat renowned as an expert on the history of the English concertina.')
+    st.image('Gaskins.jpg', caption='Robert Gaskins', width=200)
+
+page_names_to_funcs = {
+    "—": intro,
+    "John Backus": John_Backus,
+    "Bjarne Stroustrup": Bjarne_Stroustrup,
+    "Bill Gates": Bill_Gates,
+    "Larry Page": Larry_Page,
+    "Douglas Engelbart": Douglas_Engelbart,
+    "Robert Gaskins": Robert_Gaskins
+}
+
+demo_name = st.sidebar.selectbox("Select a name", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
